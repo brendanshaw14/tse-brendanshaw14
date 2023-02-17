9bcd1d3205ca476c726da017b4a09dd825be7d07
Brendan N. Shaw <f005670@plank.thayer.dartmouth.edu>
2023-02-17 16:49:40 -0500

    Finished comments, fixed error messages

--- querier/querier.c
+++ querier/querier.c
@@ -5,6 +5,8 @@ CS50- Winter 2023
 
 See querier.h for detailed descriptions of methods and functions. 
 
+Usage: ./querier <pageDirectory> <indexFile> 
+
 To use, make test. 
 */
 
@@ -13,6 +15,7 @@ To use, make test.
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <unistd.h>
 #include "../libcs50/hashtable.h"
 #include "../libcs50/webpage.h"
 #include "../libcs50/counters.h"
@@ -21,7 +24,10 @@ To use, make test.
 #include "../common/index.h"
 #include "../common/word.h"
 
-//headers
+
+/**************** file-local global variables ****************/
+
+/*******************Local Function Headers*******************/
 index_t* indexFromFile(FILE* indexFile);
 void getQueries(index_t* pageIndex, char* pageDir);
 char** tokenizeQuery(char* input, int* numWords, char* output);
@@ -35,30 +41,33 @@ void intersectHelper(void* args, const int newKey, const int newCount);
 void printQueryResult(counters_t* result, char* pageDir);
 void getNumDocs(void* counter, const int docID, const int score);
 void getHighestScore(void* argstruct, const int docID, const int score);
+int fileno(FILE *stream);
 
 
+//verifies correct number of arguments, validates pagedir and indexFile, and calls getquery
 int main(const int argc, char* argv[]){
     //check correct num args
     if (argc != 3){
-        printf("Error: Incorrect number of arguments\n");
-        exit(0);
+        fprintf(stderr, "Error: Incorrect number of arguments\n");
+        exit(1);
     }
     //validate pageDir
     if (pagedir_validate(argv[1]) != true){
-        printf("Error: invalid or nonexistent pageDir\n");
-        exit(0);
+        fprintf(stderr, "Error: invalid or nonexistent pageDir\n");
+        exit(2);
     }
     //validate indexFile
     FILE* indexFile = NULL;
     indexFile = fopen(argv[2], "r");
     if (indexFile == NULL){
-        printf("Error: Unable to open indexFile\n");
+        fprintf(stderr, "Error: Unable to open indexFile\n");
+        exit(3);
     }
-
+    //load the index from the indexFile
     index_t* index = indexFromFile(indexFile);
-
     //get the new queries from the user
     getQueries(index, argv[1]);
+    //clean up: delete index and close the file
     index_delete(index);
     fclose(indexFile);
     return 0;
@@ -66,15 +75,6 @@ int main(const int argc, char* argv[]){
 
 
 //returns a new index holding the information stored in the given indexFile
-/* Pseudo: 
-    Make a new index
-    loop through words in the file
-        scan the ints that follow
-        for each pair
-            save that pair into the index
-    return the new index
-*/
-
 index_t* indexFromFile(FILE* indexFile){
     //new index
     index_t* newIndex = index_new(file_numLines(indexFile));
@@ -92,54 +92,57 @@ index_t* indexFromFile(FILE* indexFile){
         }
         mem_free(word);
     }
+    //free mem
     mem_free(word);
     mem_free(docID);
     mem_free(num);
     return newIndex;
 }
 
-//loops through the queries inputted to stdin until there
-// are no longer queries, calling parseQuery on each line
-/* Pseudocode: 
-    get each line
-    tokenize it
-    call verifyQuery
-        if true
-            call parse query
-*/
+//loops through the queries inputted to stdin until EOF, tokenizing them and passing them to handleQuery
 void getQueries(index_t* pageIndex, char* pageDir){
     //initial print statement
-    printf("Type your search below:\n");
+    printf("Type your search:   ");
     //loop through the query lines
     char* inputLine;
     int* numWords = mem_malloc(sizeof(int));
     while ((inputLine = file_readLine(stdin)) != NULL){
-        char* output = mem_malloc(strlen(inputLine)+1);
-        char** tokenizedQuery;
-        if ((tokenizedQuery = tokenizeQuery(inputLine, numWords, output)) == NULL){
-            printf("Error: no search terms provided");
-            mem_free(tokenizedQuery);
-            return;
+        //if something was typed (as opposed to just enter)
+        if (strlen(inputLine)!=0){
+            //make space to print the line without spaces into
+            char* output = mem_malloc(strlen(inputLine)+1);
+            char** tokenizedQuery;
+            //call tokenizequery and check that terms were provided, error and get the next query if not
+            if ((tokenizedQuery = tokenizeQuery(inputLine, numWords, output)) != NULL){
+                //if input isn't from standardin (instead from a pipe), print the input query
+                if (!isatty(fileno(stdin))) {
+                    printf("%s ", inputLine);
+                }
+                //normalize the words in the query
+                normalizeQuery(tokenizedQuery, *numWords);
+                //handle the query
+                handleQuery(tokenizedQuery, *numWords, pageIndex, pageDir);
+                mem_free(tokenizedQuery);
+                mem_free(output);
+                *numWords = 0;
+            }
+            else {
+                mem_free(tokenizedQuery);
+            }
         }
-        normalizeQuery(tokenizedQuery, *numWords);
-        //handle the query
-        handleQuery(tokenizedQuery, *numWords, pageIndex, pageDir);
-        mem_free(tokenizedQuery);
+        //free the current line and ask for the next one
         mem_free(inputLine);
-        mem_free(output);
-        *numWords = 0;
-        printf("Type your search below:\n");
-        printf("_____________________________________\n");
+        printf("Type your search:   ");
     }
+    //free the rest
     mem_free(inputLine);
     mem_free(numWords);
-    
     return;
 }
 
 //takes the input line and returns it as one string with no spaces
 char** tokenizeQuery(char* input, int* numWords, char* output){
-    //make a new string to put the result into 
+    //make a new string to put the result into (MAX 200 WORDS); 
     char** outputArray = mem_malloc(200);
     bool inWord = false;
     int charIndex = 0;
@@ -163,7 +166,7 @@ char** tokenizeQuery(char* input, int* numWords, char* output){
             }
         }
         //if that character is a space
-        if (currentChar == ' '){
+        else if (currentChar == ' '){
             //if we are coming out of a word, change inword to false and add the terminator
             if (inWord == true){
                 (*numWords)++;
@@ -171,6 +174,15 @@ char** tokenizeQuery(char* input, int* numWords, char* output){
                 charIndex ++;
                 inWord = false;
             }
+        }
+        else {
+            printf("Error: invalid character(s) included in search\n");
+            return NULL;
+        }
+         //make sure that the 200 word limit hasn't been passed
+        if (*numWords >= 200){
+            printf("Error: query word limit (200) exceeded\n");
+            return NULL;
         }
     }
     //if the for loop ends while in a word, terminate the word and increment wordcount
@@ -183,10 +195,11 @@ char** tokenizeQuery(char* input, int* numWords, char* output){
 
 /* normalize all the words in the query array*/
 void normalizeQuery(char** input, int numWords){
+    //for each word in the query
     for (int i = 0; i < numWords; i ++){
-        char* currentWord = word_normalize(input[i]); 
-        strcpy(input[i], currentWord);
-        mem_free(currentWord);
+        char* currentWord = word_normalize(input[i]); //create a normalized copy
+        strcpy(input[i], currentWord);//copy that copy into the input
+        mem_free(currentWord); //then free the copy
     } 
     return;
 }
@@ -198,17 +211,17 @@ void handleQuery(char** input, int numWords, index_t* pageIndex, char* pageDir){
         printf("Error: First and last words of query cannot be operators.\n");
         return;
     }
-    //for each word in the query
     int index = 0;
     counters_t* result = counters_new();
     counters_t* temp;
     bool intersect = false;
+    //for each word in the query
     while (index < numWords){
         //if the word is and
         if(strcmp(input[index], "and") == 0){
             //make sure the next word isn't also and
             if (strcmp(input[index], input[index+1]) == 0){
-                printf("Error: found two consecutive and operators \n");
+                printf( "Error: found two consecutive and operators \n");
                 return;
             }
             index ++;
@@ -217,7 +230,7 @@ void handleQuery(char** input, int numWords, index_t* pageIndex, char* pageDir){
         else if (strcmp(input[index], "or") == 0){
             //make sure the next word isn't also and
             if (strcmp(input[index], input[index+1]) == 0){
-                printf("Error: found two consecutive or operators \n");
+                printf( "Error: found two consecutive or operators \n");
                 return;
             }
             intersect = false;
@@ -241,14 +254,12 @@ void handleQuery(char** input, int numWords, index_t* pageIndex, char* pageDir){
                 countersIntersect(temp, toIntersect);
                 temp = toIntersect;
                 index++;
-
             }
         }
     }
-    //merge temp and result
-    countersMerge(result, temp);
-    printQueryResult(result, pageDir);
-    counters_delete(result);
+    countersMerge(result, temp); //merge temp and result
+    printQueryResult(result, pageDir); //print the results
+    counters_delete(result); //delete the result
     //delete the counters
     return;
 }
@@ -260,7 +271,9 @@ void countersMerge(counters_t* result, counters_t* temp){
     return;
 }
 
-//merge helper-- function that is called upon each item in temp; receives temp-> key and temp-> count
+//merge helper-- function that is called upon each item in temp;
+// receives temp-> key and temp-> count
+//ensures adding of docs and their scores from temp into result
 void mergeHelper(void* result, const int tempKey, const int tempCount){
     //if the result doesn't already have that word, add that counter to the value of temp-> count
     if (counters_get(result, tempKey) == 0){
@@ -273,14 +286,15 @@ void mergeHelper(void* result, const int tempKey, const int tempCount){
     return;
 }
 
-//define the struct to pass as arg into countersIntersect
+//define the struct to pass as arg into countersIntersect so it can access both temp and new
 struct counterArgs {
     counters_t* temp;
     counters_t* new;
 };
+
 //intersect the counters
 void countersIntersect(counters_t* temp, counters_t* new){
-    struct counterArgs counters = {temp, new};
+    struct counterArgs counters = {temp, new}; //
     counters_iterate(new, &counters, intersectHelper);
     return;
 }
@@ -296,25 +310,31 @@ void intersectHelper(void* args, const int newKey, const int newCount){
             counters_set(counters -> new, newKey, counters_get(counters -> temp, newKey));
         }
     }
-    //otherwise set it to 0;
+    //otherwise set it to 0 (which will not be printed)
     else{ 
         counters_set(counters -> new, newKey, 0);
     }
     return;
 }
 
+//struct to hold both a maxScore and the ID for that max score to find the docId of the maxscore
 struct maxdoc {
     int maxScore;
     int ID;
 };
 
+//given a counter (result) containing docIDs and their scores
 void printQueryResult(counters_t* result, char* pageDir){
     int numMatches = 0;
+    //iterate through the result to find the number of matches (pages with a non-zero score)
     counters_iterate(result, &numMatches, getNumDocs);
+    //print if no matches were found
     if (numMatches == 0){
-        printf("No Matching Documents");
+        printf("\nNo Matching Documents");
     }
+    //if matches were found
     else{ 
+        printf("Matches %d documents (ranked):\n", numMatches);
         struct maxdoc max = {0, 0};
         for(int i = 0; i < numMatches; i ++){
             //find the highest score and its docID and store in max
@@ -323,8 +343,13 @@ void printQueryResult(counters_t* result, char* pageDir){
             char* pageDirName = mem_malloc(strlen(pageDir) + 10);
             sprintf(pageDirName, "%s/%d", pageDir, max.ID);
             FILE* fp = fopen(pageDirName, "r");
+            if (fp == NULL){
+                fprintf(stderr, "Unable to open pageDir file %d", max.ID);
+                exit(4);
+            }
             char* URL = file_readLine(fp);
-            printf("score   %d doc  %d: %s\n", max.maxScore, max.ID, URL);
+            //print that document
+            printf("\nscore   %d doc  %d: %s", max.maxScore, max.ID, URL);
             //set the current highest counter to 0 and reset args
             counters_set(result, max.ID, 0);
             max.ID = 0;
@@ -334,6 +359,7 @@ void printQueryResult(counters_t* result, char* pageDir){
             fclose(fp);
         }
     }
+    printf("\n_____________________________________\n");
     return;
 }
 //stores the number of docIDs with a non-zero score in numMatches
@@ -348,6 +374,7 @@ void getNumDocs(void* counter, const int docID, const int score){
 //sets the input arg to the key of the highest scoring doc
 void getHighestScore(void* argstruct, const int docID, const int score){
     struct maxdoc* args = argstruct;
+    //if the score is higher, then set that to the new score and save its docID
     if (score > args -> maxScore){
         args -> maxScore = score;
         args -> ID = docID;
